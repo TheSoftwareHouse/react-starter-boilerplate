a4f49bbab757896f0165ea627a93bddb2c576e69
robert-balcerowicz <robert.balcerowicz@thesoftwarehouse.pl>
2020-01-17 08:56:19 +0100

    [feature/RSS-3] Switch to using npm rather than yarn

--- src/app/help/Help.tsx
+++ src/app/help/Help.tsx
@@ -9,21 +9,21 @@ export const Help: React.FC = () => {
             <p>This project was bootstrapped with <a href="https://github.com/facebook/create-react-app">Create React App</a> and modified by TSH team.</p>
             <h3 id="available-scripts">Available Scripts</h3>
             <p>In the project directory, you can run:</p>
-            <h4 id="-yarn-start-"><code>yarn start</code></h4>
+            <h4 id="-npm-start-"><code>npm start</code></h4>
             <p>Runs the app in the development mode.<br />
-                Open <a href="http://localhost:3000">http://localhost:3000</a> to view it in the browser.</p>
+              Open <a href="http://localhost:3000">http://localhost:3000</a> to view it in the browser.</p>
             <p>The page will reload if you make edits.<br />
-                You will also see any lint errors in the console.</p>
-            <h4 id="-yarn-test-"><code>yarn test</code></h4>
+              You will also see any lint errors in the console.</p>
+            <h4 id="-npm-test-"><code>npm test</code></h4>
             <p>Launches the test runner in the interactive watch mode.<br />
-                See the section about <a href="https://facebook.github.io/create-react-app/docs/running-tests">running tests</a> for more information.</p>
-            <h4 id="-yarn-build-"><code>yarn build</code></h4>
+              See the section about <a href="https://facebook.github.io/create-react-app/docs/running-tests">running tests</a> for more information.</p>
+            <h4 id="-npm-run-build-"><code>npm run build</code></h4>
             <p>Builds the app for production to the <code>build</code> folder.<br />
-                It correctly bundles React in production mode and optimizes the build for the best performance.</p>
+              It correctly bundles React in production mode and optimizes the build for the best performance.</p>
             <p>The build is minified and the filenames include the hashes.<br />
-                Your app is ready to be deployed!</p>
+              Your app is ready to be deployed!</p>
             <p>See the section about <a href="https://facebook.github.io/create-react-app/docs/deployment">deployment</a> for more information.</p>
-            <h4 id="-yarn-eject-"><code>yarn eject</code></h4>
+            <h4 id="-npm-run-eject-"><code>npm run eject</code></h4>
             <p><strong>Note: this is a one-way operation. Once you <code>eject</code>, you can’t go back!</strong></p>
             <p>If you aren’t satisfied with the build tool and configuration choices, you can <code>eject</code> at any time. This command will remove the single build dependency from your project.</p>
             <p>Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except <code>eject</code> will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.</p>
