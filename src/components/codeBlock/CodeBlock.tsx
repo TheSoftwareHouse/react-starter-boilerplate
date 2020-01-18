2fa1522947ff1aea7e5ca39c606484b5030bff18
Lukasz Golder <lukasz.golder@allegro.pl>
2020-01-18 19:55:36 +0100

    RSS-8 | Replace joinClassNames with clsx

--- src/components/codeBlock/CodeBlock.tsx
+++ src/components/codeBlock/CodeBlock.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-
-import { joinClassNames } from 'helpers/joinClassNames/joinClassNames';
+import classNames from 'clsx';
 
 import './CodeBlock.css';
 import { CodeBlockPropsType } from './CodeBlock.types';
 
 export const CodeBlock: React.FC<CodeBlockPropsType> = ({ className, children, ...rest }) => (
-  <p className={joinClassNames('code-block', className)} {...rest}>
+  <p className={classNames('code-block', className)} {...rest}>
     <code>{children}</code>
   </p>
 );
