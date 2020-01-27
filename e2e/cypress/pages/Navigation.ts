6be1e606dbd63f7af2f64ed553c58ecb6527b5ef
Jakub Musik <jakubmusik@thesoftwarehouse.pl>
2020-01-27 08:47:31 +0100

    RSS-12 | Resolve conflicts

--- e2e/cypress/pages/Navigation.ts
+++ e2e/cypress/pages/Navigation.ts
@@ -1,20 +1,20 @@
-import { RoutesEnum } from '../../../src/navigation/routes.enum';
+import { AppRoute } from '../../../src/app/routes/AppRoute.enum';
 
 class Navigation {
   goToAbout(): void {
-    cy.get(`a[href*="${RoutesEnum.about}"]`).click();
+    cy.get(`a[href*="${AppRoute.about}"]`).click();
   }
 
   goToHelp(): void {
-    cy.get(`a[href*="${RoutesEnum.help}"]`).click();
+    cy.get(`a[href*="${AppRoute.help}"]`).click();
   }
 
   get aboutLink(): string {
-    return `${Cypress.env().baseUrl}${RoutesEnum.about}`;
+    return `${Cypress.env().baseUrl}${AppRoute.about}`;
   }
 
   get helpLink(): string {
-    return `${Cypress.env().baseUrl}${RoutesEnum.help}`;
+    return `${Cypress.env().baseUrl}${AppRoute.help}`;
   }
 }
 
