186b506d3811f2053f303a471af9418faffe384b
Jakub Musik <jakubmusik@thesoftwarehouse.pl>
2020-02-10 10:13:44 +0100

    Add miragejs for mocking

--- src/api/mocks/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './authorizeMock/authorizeMock';
-export * from './fetchCurrentUserMock/fetchCurrentUserMock';
--- src/context/client/clientContextController/clientContextController.tsx
+++ src/context/client/clientContextController/clientContextController.tsx
@@ -2,23 +2,11 @@ import React, { useMemo } from 'react';
 import { ClientContextProvider, createClient, RequestInterceptor, ResponseInterceptor } from 'react-fetching-library';
 
 import { requestAuthInterceptor, requestHostInterceptor } from 'api/interceptors';
-import { authorizeMock, fetchCurrentUserMock } from 'api/mocks';
 import { useAuthState } from 'hooks';
-
 import { ClientProviderProps } from './clientContextController.types';
 
 const requestInterceptors: RequestInterceptor[] = [];
 const responseInterceptors: ResponseInterceptor[] = [];
-
-if (+(process.env.REACT_APP_CI || 0) === 1 || process.env.NODE_ENV === 'development') {
-  responseInterceptors.push(authorizeMock);
-  // eslint-disable-next-line no-console
-  console.log('authorizeMock has been added!');
-
-  responseInterceptors.push(fetchCurrentUserMock);
-  // eslint-disable-next-line no-console
-  console.log('fetchCurrentUserMock has been added!');
-}
 
 export const ClientContextController: React.FC<ClientProviderProps> = ({ children }) => {
   const { accessToken } = useAuthState();
