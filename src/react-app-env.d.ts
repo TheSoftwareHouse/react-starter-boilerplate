95105be24c28cabdeb38b6797951e960efbde7ea
Jakub Musik <jakubmusik@thesoftwarehouse.pl>
2020-02-13 12:13:56 +0100

    RSS-18 | Add types to env values

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="react-scripts" />
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: 'development' | 'production' | 'test';
+    REACT_APP_DEFAULT_LOCALE: string;
+    REACT_APP_API_URL: string;
+  }
+}
