ccf6ac993164e9402cc77178dc3800258837ea83
Łukasz Tyszkiewicz <lukasz.tyszkiewicz@tsh.io>
2021-11-17 08:44:05 +0100

    [ZN-119] remove not necessary httpMethod type

--- src/api/types/types.ts
+++ src/api/types/types.ts
@@ -1,7 +1,5 @@
 import { AxiosResponse } from 'axios';
 
-export type HTTPMethod = 'GET' | 'DELETE' | 'HEAD' | 'OPTIONS' | 'POST' | 'PUT' | 'PATCH' | 'PURGE' | 'LINK' | 'UNLINK';
-
 export type MutationHTTPMethod = 'DELETE' | 'POST' | 'PUT' | 'PATCH';
 
 export type ClientResponse<TResponse = unknown> = AxiosResponse<TResponse>;
