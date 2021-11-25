1cdf2e6f89f32f7d672d557418d5ac06f6430c27
Bartosz Gajos <bartosz.gajos@tsh.io>
2021-11-25 15:52:47 +0100

    [ZN-180] add debug mode to watcher

--- e2e/scripts/watcher/watcher.ts
+++ e2e/scripts/watcher/watcher.ts
@@ -4,13 +4,19 @@ import { watch } from 'chokidar';
 
 import config from './watcher.config';
 import { Flag } from '../scripts.types';
-import { wrapTopLevelAwait } from '../scripts.utils';
+import { debug, wrapTopLevelAwait } from '../scripts.utils';
 
 const CY_INTEGRATION_DIR = argv[Flag.integrationDir] ?? config.testsDir;
 const TEST_FILES_PATTERN = argv[Flag.testFilesPattern] ?? config.testFilesPattern;
 const BROWSER = argv[Flag.browser] ?? config.browser;
+const DEBUG = argv[Flag.debug];
+
 let isRunning = false;
 $.verbose = false;
+
+debug(DEBUG, 'Integration dir:', CY_INTEGRATION_DIR);
+debug(DEBUG, 'Test files pattern:', TEST_FILES_PATTERN);
+debug(DEBUG, 'Browser:', BROWSER);
 
 console.log(`Look for '${chalk.bgGrey(TEST_FILES_PATTERN)}' files that have changed since last commit. Trigger by changes in '${chalk.bgGrey(CY_INTEGRATION_DIR)}'`);
 
@@ -29,17 +35,18 @@ const runTests = async (_specs: string[]) => {
 const getChangedSpecs = async () => {
   console.log(`\nLooking for changed test files...`);
   const changedFiles = await $`git diff --name-only HEAD`;
+  debug(DEBUG, 'All changed files from last commit:', changedFiles.stdout.split('\n'));
+
   const onlySpecFiles = changedFiles.stdout
     .split('\n')
     .filter(file => file.match(TEST_FILES_PATTERN))
-    .map(file => {
-      console.log(file)
-      return file.replace(config.pathFromGitToCurrentPackageJson, './');
-    });
+    .map(file => file.replace(config.pathFromGitToCurrentPackageJson, './'));
+
   if (onlySpecFiles.length > 0) {
+    debug(DEBUG, 'Selected test files:', onlySpecFiles);
     return onlySpecFiles;
   } else {
-    console.log(`\nNo changes were found since last commit`);
+    console.log(`\nNo changes were found in test files since last commit`);
     return [];
   }
 };
