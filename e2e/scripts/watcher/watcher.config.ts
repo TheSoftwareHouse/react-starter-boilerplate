a4df93438f27c2fd214eba866c695853f031c80e
Bartosz Gajos <bartosz.gajos@tsh.io>
2021-11-25 12:50:00 +0100

    [ZN-180] change file pattern

--- e2e/scripts/watcher/watcher.config.ts
+++ e2e/scripts/watcher/watcher.config.ts
@@ -3,7 +3,7 @@ import { Config } from '../scripts.types';
 const config: Config = {
   pathFromGitToCurrentPackageJson: 'e2e/',
   testsDir: `./cypress/integration`,
-  testFilesPattern: 'e2e\\/.*\\/integration\\/.*?(?=.test).*?.ts',
+  testFilesPattern: 'e2e\\/cypress\\/integration\\/.*?(?=.test).*?.ts',
   browser: 'chrome',
 };
 export default config;
