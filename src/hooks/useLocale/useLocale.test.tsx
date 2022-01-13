8a13b8ba13ebbaa5d67a804015f10631bbc5ecaa
Marcin Jenczmyk <marcin.jenczmyk@tsh.io>
2022-01-13 13:19:22 +0100

    [ZN-236] Disable react/react-in-jsx-scope ESlint rule

--- src/hooks/useLocale/useLocale.test.tsx
+++ src/hooks/useLocale/useLocale.test.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { renderHook } from '@testing-library/react-hooks';
 import { IntlProvider } from 'react-intl';
 
--- src/ui/loader/Loader.tsx
+++ src/ui/loader/Loader.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export const Loader = () => {
   return <div>Loading...</div>;
 };
