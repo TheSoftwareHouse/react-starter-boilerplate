a580aa222d7be44985958190a4fdc854fd680ba6
Bartosz Gajos <bartosz.gajos@tsh.io>
2022-03-21 16:11:24 +0100

    [ZN-283] remove underscore

--- src/context/apiClient/apiClientContext/ApiClientContext.types.ts
+++ src/context/apiClient/apiClientContext/ApiClientContext.types.ts
@@ -7,7 +7,7 @@ export type ApiClientContextValue = {
   queryFn: QueryFunction;
   mutationFn: <TParams, TData>(mutation: MutationFn<TParams, TData>) => MutationFunction<TData, TParams>;
   infiniteQueryFn: <TArgs, TParams, TResponse, TError>(
-    _query: InfiniteQueryFn<TArgs, TParams, TResponse>,
+    query: InfiniteQueryFn<TArgs, TParams, TResponse>,
     options?: UseInfiniteQueryOptions<TArgs, TParams, TError, TResponse>,
   ) => QueryFunction<TParams>;
 };
