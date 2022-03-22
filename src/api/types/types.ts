349baa986a6a51ee9fc3c61048a6fdede684d37b
Bartosz Gajos <bartosz.gajos@tsh.io>
2022-03-22 13:52:23 +0100

    [ZN-283] remove unnecessary types

--- src/api/types/types.ts
+++ src/api/types/types.ts
@@ -1,9 +1,1 @@
-import { AxiosResponse } from 'axios';
-
 export type MutationHTTPMethod = 'DELETE' | 'POST' | 'PUT' | 'PATCH';
-
-// using Axios
-export type ClientResponse<TResponse = unknown> = AxiosResponse<TResponse>;
-
-//using Ky
-// export type ClientResponse<TResponse = unknown> = TResponse;
