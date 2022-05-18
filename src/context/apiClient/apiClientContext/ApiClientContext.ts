6e2eed789c6226a9d83a5e775ff80facf2c5befb
Bartosz Gajos <bartosz.gajos@tsh.io>
2022-05-18 09:44:24 +0200

    [ZN-354] add startPage option to infinite query

--- src/context/apiClient/apiClientContext/ApiClientContext.ts
+++ src/context/apiClient/apiClientContext/ApiClientContext.ts
@@ -1,22 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { createContext } from 'react';
-import { QueryFunction } from 'react-query';
 
 import { ApiClientContextValue } from './ApiClientContext.types';
 
-const mockedInitialContextValue: ApiClientContextValue = {
-  queryFn:
-    <TData>() =>
-    () =>
-      Promise.resolve({} as TData),
-  mutationFn:
-    <TParams, TData>() =>
-    () =>
-      Promise.resolve({} as TData),
-  infiniteQueryFn:
-    <TArgs, TParams, TResponse, TError>(): QueryFunction<TParams> =>
-    () =>
-      Promise.resolve({} as TParams),
-};
-
-export const ApiClientContext = createContext<ApiClientContextValue>(mockedInitialContextValue);
+export const ApiClientContext = createContext<ApiClientContextValue | undefined>(undefined);
