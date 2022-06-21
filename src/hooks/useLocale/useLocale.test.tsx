2ae273940f4c96bb3b7bccbfa27ffc3a79cd66ff
Rafał Bąbiński <rafal.babinski@tsh.io>
2022-06-21 14:29:26 +0200

    [ZN-361] import renderHook method from @testing-library/react

--- src/hooks/useLocale/useLocale.test.tsx
+++ src/hooks/useLocale/useLocale.test.tsx
@@ -1,20 +1,21 @@
-import { renderHook } from '@testing-library/react-hooks';
 import { IntlProvider } from 'react-intl';
+
+import { renderHook } from 'tests';
 
 import { useLocale } from './useLocale';
 
 describe('useLocale', () => {
   test('throws when locale context is unavailable', () => {
-    const { result } = renderHook(() => useLocale(), {
-      wrapper: ({ children }) => (
-        <IntlProvider onError={() => {}} locale="">
-          {children}
-        </IntlProvider>
-      ),
-    });
-
-    expect(result.error).toEqual(
-      Error('LocaleContext is unavailable, make sure you are using LocaleContextController'),
-    );
+    try {
+      renderHook(() => useLocale(), {
+        wrapper: ({ children }) => (
+          <IntlProvider onError={() => {}} locale="">
+            {children}
+          </IntlProvider>
+        ),
+      });
+    } catch (error) {
+      expect(error).toEqual(Error('LocaleContext is unavailable, make sure you are using LocaleContextController'));
+    }
   });
 });
