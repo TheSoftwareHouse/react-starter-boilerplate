be6d95f93c29d1e33265abbe3403e172ce2f63da
Bartosz Gajos <bartosz.gajos@tsh.io>
2022-07-26 09:27:11 +0200

    [ZN-329] build qs like without interpolation

--- src/api/actions/auth/authActions.ts
+++ src/api/actions/auth/authActions.ts
@@ -3,13 +3,13 @@ import { InfiniteQueryFn } from 'hooks/useInfiniteQuery/useInfiniteQuery.types';
 
 import { GetUsersResponse, LoginMutationArguments, LoginMutationResponse } from './authActions.types';
 
-export const meQueryKey = 'me';
+export const meQueryKey = '/me';
 export const usersQueryKey = 'users';
 export const loginQueryKey = 'login';
 
 export const loginMutation: MutationFn<LoginMutationArguments, LoginMutationResponse> = (body) => {
   return {
-    endpoint: 'authorize',
+    endpoint: '/authorize',
     method: 'POST',
     params: body,
   };
@@ -17,7 +17,7 @@ export const loginMutation: MutationFn<LoginMutationArguments, LoginMutationResp
 
 export const getInfiniteUsersQuery: InfiniteQueryFn<{ count: number }, GetUsersResponse> = (args) => {
   return {
-    endpoint: `users`,
+    endpoint: `/users`,
     args,
   };
 };
