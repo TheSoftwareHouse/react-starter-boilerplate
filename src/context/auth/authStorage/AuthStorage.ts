59f38308b4a9953b6741c685fea9e22a2a44dbc1
Bartosz Gajos <bartosz.gajos@tsh.io>
2022-07-29 08:27:15 +0200

    [ZN-329] pass storage to constructor

--- src/context/auth/authStorage/AuthStorage.ts
+++ src/context/auth/authStorage/AuthStorage.ts
@@ -4,27 +4,20 @@ const ACCESS_TOKEN_KEY = 'accessToken';
 const REFRESH_TOKEN_KEY = 'refreshToken';
 const EXPIRES_KEY = 'expires';
 
-const storage: Storage = {
-  getItem: (key: string) => sessionStorage.getItem(key),
-  setItem: (key: string, value: string) => sessionStorage.setItem(key, value),
-  removeItem: (key: string) => sessionStorage.removeItem(key),
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  onError: (error: unknown) => {
-    // handle errors here
-  },
-};
-
 class AuthStorage {
   private _accessToken: string | null = null;
   private _refreshToken: string | null = null;
   private _expires: number | null = null;
+  private _storage: Storage | null = null;
 
-  constructor() {
+  constructor(_storage: Storage) {
     try {
-      this.accessToken = storage.getItem(ACCESS_TOKEN_KEY);
-      this.refreshToken = storage.getItem(REFRESH_TOKEN_KEY);
-      this.expires = Number(storage.getItem(EXPIRES_KEY));
+      this._storage = _storage;
+      this.accessToken = _storage.getItem(ACCESS_TOKEN_KEY);
+      this.refreshToken = _storage.getItem(REFRESH_TOKEN_KEY);
+      this.expires = Number(_storage.getItem(EXPIRES_KEY));
     } catch (error) {
+      this._storage = null;
       this.accessToken = null;
       this.refreshToken = null;
       this.expires = null;
@@ -40,12 +33,12 @@ class AuthStorage {
 
     try {
       if (typeof value === 'string') {
-        storage.setItem(ACCESS_TOKEN_KEY, value);
+        this._storage?.setItem(ACCESS_TOKEN_KEY, value);
       } else {
-        storage.removeItem(ACCESS_TOKEN_KEY);
+        this._storage?.removeItem(ACCESS_TOKEN_KEY);
       }
     } catch (error) {
-      storage.onError(error);
+      this._storage?.onError(error);
     }
   }
 
@@ -58,12 +51,12 @@ class AuthStorage {
 
     try {
       if (typeof value === 'string') {
-        storage.setItem(REFRESH_TOKEN_KEY, value);
+        this._storage?.setItem(REFRESH_TOKEN_KEY, value);
       } else {
-        storage.removeItem(REFRESH_TOKEN_KEY);
+        this._storage?.removeItem(REFRESH_TOKEN_KEY);
       }
     } catch (error) {
-      storage.onError(error);
+      this._storage?.onError(error);
     }
   }
 
@@ -76,14 +69,24 @@ class AuthStorage {
 
     try {
       if (typeof value === 'number') {
-        storage.setItem(EXPIRES_KEY, value.toString());
+        this._storage?.setItem(EXPIRES_KEY, value.toString());
       } else {
-        storage.removeItem(EXPIRES_KEY);
+        this._storage?.removeItem(EXPIRES_KEY);
       }
     } catch (error) {
-      storage.onError(error);
+      this._storage?.onError(error);
     }
   }
 }
 
-export const authStorage = new AuthStorage();
+const storage: Storage = {
+  getItem: (key: string) => sessionStorage.getItem(key),
+  setItem: (key: string, value: string) => sessionStorage.setItem(key, value),
+  removeItem: (key: string) => sessionStorage.removeItem(key),
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  onError: (error: unknown) => {
+    // handle errors here
+  },
+};
+
+export const authStorage = new AuthStorage(storage);
--- src/context/auth/authStorage/AuthStorage.types.ts
+++ src/context/auth/authStorage/AuthStorage.types.ts
@@ -1,6 +1,6 @@
-export type Storage<TItem = string | null> = {
+export interface Storage<TItem = string | null> {
   getItem: (key: string) => TItem;
   setItem: (key: string, value: string) => void;
   removeItem: (key: string) => void;
   onError: (error: unknown) => void;
-};
+}
