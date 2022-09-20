8b9529f6c2eb0db31bac25e2603c0b173bcf7463
Kacper Witas <kacper.witas@tsh.io>
2022-09-20 08:59:31 +0200

    chore(ZN-376): loading status abstraction

--- src/hooks/useQuery/useQuery.ts
+++ src/hooks/useQuery/useQuery.ts
@@ -6,8 +6,10 @@ import { useApiClient } from '../useApiClient/useApiClient';
 export const useQuery = <TData = unknown, TError = unknown>(
   queryKey: QueryKey,
   options?: UseQueryOptions<TData, TError>,
-): UseQueryResult<TData, TError> => {
+): UseQueryResult<TData, TError> & { isLoadingAndEnabled: boolean } => {
   const { queryFn } = useApiClient();
   const _queryFn = useMemo(() => queryFn<TData>(), [queryFn]);
-  return useRqQuery<TData, TError, TData, QueryKey>(queryKey, _queryFn, options);
+  const result = useRqQuery<TData, TError, TData, QueryKey>(queryKey, _queryFn, options);
+
+  return { ...result, isLoadingAndEnabled: result.isLoading && result.fetchStatus !== 'idle' };
 };
