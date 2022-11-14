f9aa72d93894ee5eadfae96310c55673d3df2b70
Kacper Witas <kacper.witas@tsh.io>
2022-11-14 21:46:16 +0100

    feat(ZN-390): change naming

--- src/types/params-utils.ts
+++ src/types/params-utils.ts
@@ -2,12 +2,12 @@ import { NavigateOptions as ReactNavigateOptions } from 'react-router-dom';
 
 import { Split } from './split';
 
-export type GetParams<Path extends string> = ParamsOnly<Split<Path, '/'>>;
-
-export type PathParams<Path extends string> = Record<GetParams<Path>, string>;
-
 export type ParamsOnly<S extends string> = S extends `:${infer Rest}` ? Rest : never;
 
-export type HasParams<Path extends string> = GetParams<Path> extends never ? false : true;
+export type ParamsKeys<Path extends string> = ParamsOnly<Split<Path, '/'>>;
+
+export type PathParams<Path extends string> = Record<ParamsKeys<Path>, string>;
+
+export type HasParams<Path extends string> = ParamsKeys<Path> extends never ? false : true;
 
 export type NavigateOptions<Params extends object> = { params: Params } & ReactNavigateOptions;
