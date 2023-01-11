bf7d5e3002606f2a4e732029be89f3f9f7f638e4
Filip Lipiński <filip.lipinski@tsh.io>
2023-01-11 08:25:51 +0100

    feat(ZN-387): change api url mock to avoid fetch error on mocked endpoints

--- src/context/apiClient/apiClientContextController/interceptors/responseInterceptors.ts
+++ src/context/apiClient/apiClientContextController/interceptors/responseInterceptors.ts
@@ -1,6 +1,5 @@
 import { AxiosResponse } from 'axios';
 
-export const responseSuccessInterceptor = (response: AxiosResponse) =>
-  response.headers['content-type'] === 'application/json' ? response : Promise.reject(response);
+export const responseSuccessInterceptor = (response: AxiosResponse) => response;
 
 export const responseFailureInterceptor = async (error: any) => Promise.reject(error); // eslint-disable-line @typescript-eslint/no-explicit-any
