39585878e6c04fa5e9f9e49754290a7efa22e941
Przemyslaw Bak <przemyslaw.bak@tsh.io>
2023-08-23 10:10:20 +0200

    feat: handle interceptors error

--- src/context/apiClient/apiClientContextController/interceptors/requestInterceptors.ts
+++ src/context/apiClient/apiClientContextController/interceptors/requestInterceptors.ts
@@ -1,17 +1,43 @@
-import { AxiosRequestHeaders, InternalAxiosRequestConfig } from 'axios';
+import axios, { InternalAxiosRequestConfig } from 'axios';
+import decode from 'jwt-decode';
 
 import { authStorage } from 'context/auth/authStorage/AuthStorage';
+import { RefreshMutationResponse } from 'api/actions/auth/authActions.types';
 
-export const requestSuccessInterceptor = (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => {
-  if (authStorage.accessToken) {
+export const requestSuccessInterceptor = async (config: InternalAxiosRequestConfig) => {
+  if (authStorage.accessToken && authStorage.expires !== null) {
+    const secondsSinceEpoch = Math.round(new Date().getTime() / 1000);
+    const isTokenExpired = secondsSinceEpoch >= authStorage.expires;
+
+    if (isTokenExpired) {
+      await axios
+        .post<RefreshMutationResponse>(`${import.meta.env.VITE_SECURITY_API_URL}/users/refresh-token`, {
+          accessToken: authStorage.accessToken,
+          refreshToken: authStorage.refreshToken,
+        })
+        .then(({ data }) => {
+          const { exp } = decode(data.accessToken) as { exp: number };
+
+          authStorage.accessToken = data.accessToken;
+          authStorage.expires = exp;
+          authStorage.refreshToken = data.refreshToken;
+        })
+        .catch(() => {
+          authStorage.accessToken = null;
+          authStorage.expires = null;
+          authStorage.refreshToken = null;
+        });
+    }
+
     return {
       ...config,
-      withCredentials: true,
       headers: {
         ...config.headers,
         Authorization: `Bearer ${authStorage.accessToken}`,
-      } as AxiosRequestHeaders,
+      },
+      withCredentials: false,
     };
   }
+
   return config;
 };
--- src/context/apiClient/apiClientContextController/interceptors/responseInterceptors.ts
+++ src/context/apiClient/apiClientContextController/interceptors/responseInterceptors.ts
@@ -1,5 +1,64 @@
-import { AxiosResponse } from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import decode from 'jwt-decode';
+
+import { RefreshMutationResponse } from 'api/actions/auth/authActions.types';
+import { authStorage } from 'context/auth/authStorage/AuthStorage';
+import { APIErrorOutput, ExtendedAxiosRequestConfig } from 'api/types/types';
+import { ErrorDataStrategy, ApiErrorHandler } from 'api/axios/ApiErrorStrategy';
+
+const errorResponseStrategy = new ErrorDataStrategy();
+const responseErrorHandler = new ApiErrorHandler(errorResponseStrategy);
 
 export const responseSuccessInterceptor = (response: AxiosResponse) => response;
 
-export const responseFailureInterceptor = async (error: any) => Promise.reject(error); // eslint-disable-line @typescript-eslint/no-explicit-any
+export const responseFailureInterceptor = async (error: AxiosError): Promise<APIErrorOutput> => {
+  const getErrorResponse = responseErrorHandler.handleError(error);
+  const status = getErrorResponse.status || 500;
+  const errorData = {
+    status,
+    code: getErrorResponse.code,
+    message: getErrorResponse.message,
+  };
+
+  const originalRequest = error.config as ExtendedAxiosRequestConfig;
+
+  if (status === 401 && originalRequest?._retry) {
+    authStorage.accessToken = null;
+    authStorage.expires = null;
+    authStorage.refreshToken = null;
+
+    window.location.replace('/login');
+
+    return Promise.reject(errorData);
+  }
+
+  if (status === 401 && originalRequest) {
+    originalRequest._retry = true;
+
+    try {
+      const { data } = await axios.post<RefreshMutationResponse>(
+        `${import.meta.env.VITE_SECURITY_API_URL}/users/refresh-token`,
+        {
+          accessToken: authStorage.accessToken,
+          refreshToken: authStorage.refreshToken,
+        },
+      );
+      const { exp } = decode(data.accessToken) as { exp: number };
+
+      authStorage.accessToken = data.accessToken;
+      authStorage.expires = exp;
+      authStorage.refreshToken = data.refreshToken;
+
+      return axios(originalRequest);
+    } catch {
+      authStorage.accessToken = null;
+      authStorage.expires = null;
+      authStorage.refreshToken = null;
+      window.location.replace('/login');
+
+      return Promise.reject(errorData);
+    }
+  }
+
+  return Promise.reject(errorData);
+};
