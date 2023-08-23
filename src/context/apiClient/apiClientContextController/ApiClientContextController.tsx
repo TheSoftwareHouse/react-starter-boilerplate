c694f54b9c7812144735d8426e2403a7e1576036
Przemyslaw Bak <przemyslaw.bak@tsh.io>
2023-08-23 10:07:37 +0200

    feat: add mutation and query cache to handle errors globally

--- src/context/apiClient/apiClientContextController/ApiClientContextController.tsx
+++ src/context/apiClient/apiClientContextController/ApiClientContextController.tsx
@@ -1,15 +1,40 @@
 import { useMemo } from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { MutationCache, QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { ApiClientContext } from 'context/apiClient/apiClientContext/ApiClientContext';
 import { ApiClientContextValue } from 'context/apiClient/apiClientContext/ApiClientContext.types';
 import axiosClient from 'api/axios';
+import { useHandleQueryErrors } from 'hooks/useHandleQueryErrors/useHandleQueryErrors';
+import { ClientErrorResponse, ExtendedQueryMeta } from 'api/types/types';
 
 import { ApiClientControllerProps } from './ApiClientContextController.types';
 
 export const ApiClientContextController = ({ children }: ApiClientControllerProps) => {
+  const { handleErrors, shouldHandleGlobalError } = useHandleQueryErrors();
+
+  const mutationCache = new MutationCache({
+    onError: (error, variables, context, mutation) => {
+      const { error: metaError } = mutation.meta as ExtendedQueryMeta;
+      shouldHandleGlobalError(metaError, (error as ClientErrorResponse)?.code) &&
+        handleErrors(error as ClientErrorResponse);
+    },
+  });
+  const queryCache = new QueryCache({
+    onError: (error, query) => {
+      const { error: metaError } = query.meta as ExtendedQueryMeta;
+      shouldHandleGlobalError(metaError, (error as ClientErrorResponse)?.code) &&
+        handleErrors(error as ClientErrorResponse);
+    },
+  });
+
   const queryClient = useMemo(
-    () => new QueryClient({ defaultOptions: { queries: { refetchOnWindowFocus: false } } }),
+    () =>
+      new QueryClient({
+        defaultOptions: { queries: { refetchOnWindowFocus: false } },
+        mutationCache,
+        queryCache,
+      }),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     [],
   );
 
