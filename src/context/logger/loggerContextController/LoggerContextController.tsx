eab975774bd64cd9b6dfda92aa03804c56198d14
Przemyslaw Bak <przemyslaw.bak@tsh.io>
2023-08-28 12:41:49 +0200

    refactor: remove useMemo and pass state as value in provider

--- src/context/logger/loggerContextController/LoggerContextController.tsx
+++ src/context/logger/loggerContextController/LoggerContextController.tsx
@@ -1,20 +1,12 @@
-import { useMemo, useState } from 'react';
+import { useState } from 'react';
 
 import { LoggerContext } from '../loggerContext/LoggerContext';
 import { loggerService } from '../loggerService/LoggerService';
-import { LoggerContextValueType } from '../loggerContext/LoggerContext.types';
 
 import { LoggerContextControllerProps } from './LoggerContextController.types';
 
 export const LoggerContextController = ({ children }: LoggerContextControllerProps) => {
   const [state] = useState(loggerService.logger);
 
-  const value: LoggerContextValueType = useMemo(
-    () => ({
-      ...state,
-    }),
-    [state],
-  );
-
-  return <LoggerContext.Provider value={value}>{children}</LoggerContext.Provider>;
+  return <LoggerContext.Provider value={state}>{children}</LoggerContext.Provider>;
 };
