cee459fa8a49a010466b9ab278b3b58b6fe739c1
SkrzypMajster <skrzypmajster@gmail.com>
2023-09-01 13:13:38 +0200

    feat: [ZN-411] Add API mutation plop command

--- plopfile.js
+++ plopfile.js
@@ -8,6 +8,7 @@ const componentTypes = {
   CUSTOM_HOOK: 'Custom hook',
   API_ACTIONS: 'API actions collection',
   API_QUERY: 'API query',
+  API_MUTATION: 'API mutation',
   REACT_CONTEXT: 'React Context',
 };
 
@@ -222,55 +223,114 @@ const apiQueryGenerator = () => ({
   }
 });
 
-const reactContextGenerator = () => ({
-  description: componentTypes.REACT_CONTEXT,
+const apiMutationGenerator = () => ({
+  description: componentTypes.API_MUTATION,
   prompts: [
+    {
+      type: "list",
+      name: "collection",
+      message: "API actions collection name?",
+      default: apiActionCollections[0],
+      choices: apiActionCollections.map((collection) => ({ name: collection, value: collection })),
+    },
     {
       type: 'input',
       name: 'name',
-      message: 'context name',
+      message: 'API query action name?',
+    },
+    {
+      type: 'input',
+      name: 'path',
+      message: 'API query action path?',
+    },
+    {
+      type: "list",
+      name: "method",
+      message: "Mutation action method?",
+      default: "post",
+      choices: [
+        { name: "post", value: "post" },
+        { name: "delete", value: "delete" },
+        { name: "patch", value: "patch" },
+        { name: "put", value: "put" },
+      ],
     }
   ],
   actions: function() {
     return [
       {
-        type: 'add',
-        path: 'src/context/{{camelCase name}}/{{camelCase name}}Context/{{pascalCase name}}Context.ts',
-        templateFile: 'plop-templates/context/Context.hbs',
+        type: 'modify',
+        path: 'src/api/actions/{{collection}}/{{collection}}.types.ts',
+        pattern: getPlaceholderPattern('API_ACTION_TYPES'),
+        templateFile: 'plop-templates/apiMutation/apiMutation.types.hbs',
       },
       {
-        type: 'add',
-        path: 'src/context/{{camelCase name}}/{{camelCase name}}Context/{{pascalCase name}}Context.types.ts',
-        templateFile: 'plop-templates/context/Context.types.hbs',
+        type: 'modify',
+        path: 'src/api/actions/{{collection}}/{{collection}}.mutations.ts',
+        pattern: getPlaceholderPattern('MUTATION_TYPE_IMPORTS'),
+        template: '{{pascalCase name}}Payload,\n\t{{pascalCase name}}Response,\n\t$1',
       },
       {
-        type: 'add',
-        path: 'src/context/{{camelCase name}}/{{camelCase name}}Context/{{pascalCase name}}Context.test.tsx',
-        templateFile: 'plop-templates/context/Context.test.hbs',
-      },
-      {
-        type: 'add',
-        path: 'src/context/{{camelCase name}}/{{camelCase name}}ContextController/{{pascalCase name}}ContextController.tsx',
-        templateFile: 'plop-templates/context/ContextController.hbs',
-      },
-      {
-        type: 'add',
-        path: 'src/context/{{camelCase name}}/{{camelCase name}}ContextController/{{pascalCase name}}ContextController.types.ts',
-        templateFile: 'plop-templates/context/ContextController.types.hbs',
-      },
-      {
-        type: 'add',
-        path: 'src/hooks/use{{pascalCase name}}/use{{pascalCase name}}.ts',
-        templateFile: 'plop-templates/context/useContext.hbs',
-      },
-      {
-        type: 'add',
-        path: 'src/hooks/use{{pascalCase name}}/use{{pascalCase name}}.test.tsx',
-        templateFile: 'plop-templates/context/useContext.test.hbs',
-      },
-    ];
-  },
+        type: 'modify',
+        path: 'src/api/actions/{{collection}}/{{collection}}.mutations.ts',
+        pattern: getPlaceholderPattern('MUTATION_FUNCTIONS_SETUP'),
+        templateFile: 'plop-templates/apiMutation/apiMutation.hbs',
+      }
+    ]
+  }
 });
+
+const reactContextGenerator = () => (
+  {
+    description: componentTypes.REACT_CONTEXT,
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'context name',
+      }
+    ],
+    actions: function() {
+      return [
+        {
+          type: 'add',
+          path: 'src/context/{{camelCase name}}/{{camelCase name}}Context/{{pascalCase name}}Context.ts',
+          templateFile: 'plop-templates/context/Context.hbs',
+        },
+        {
+          type: 'add',
+          path: 'src/context/{{camelCase name}}/{{camelCase name}}Context/{{pascalCase name}}Context.types.ts',
+          templateFile: 'plop-templates/context/Context.types.hbs',
+        },
+        {
+          type: 'add',
+          path: 'src/context/{{camelCase name}}/{{camelCase name}}Context/{{pascalCase name}}Context.test.tsx',
+          templateFile: 'plop-templates/context/Context.test.hbs',
+        },
+        {
+          type: 'add',
+          path: 'src/context/{{camelCase name}}/{{camelCase name}}ContextController/{{pascalCase name}}ContextController.tsx',
+          templateFile: 'plop-templates/context/ContextController.hbs',
+        },
+        {
+          type: 'add',
+          path: 'src/context/{{camelCase name}}/{{camelCase name}}ContextController/{{pascalCase name}}ContextController.types.ts',
+          templateFile: 'plop-templates/context/ContextController.types.hbs',
+        },
+        {
+          type: 'add',
+          path: 'src/hooks/use{{pascalCase name}}/use{{pascalCase name}}.ts',
+          templateFile: 'plop-templates/context/useContext.hbs',
+        },
+        {
+          type: 'add',
+          path: 'src/hooks/use{{pascalCase name}}/use{{pascalCase name}}.test.tsx',
+          templateFile: 'plop-templates/context/useContext.test.hbs',
+        },
+      ];
+    }
+  }
+);
 
 module.exports = function(plop) {
   plop.setPrompt('directory', promptDirectory);
@@ -278,5 +338,6 @@ module.exports = function(plop) {
   plop.setGenerator(componentTypes.CUSTOM_HOOK, customHookGenerator());
   plop.setGenerator(componentTypes.API_ACTIONS, apiActionsGenerator());
   plop.setGenerator(componentTypes.API_QUERY, apiQueryGenerator());
+  plop.setGenerator(componentTypes.API_MUTATION, apiMutationGenerator());
   plop.setGenerator(componentTypes.REACT_CONTEXT, reactContextGenerator());
 };
