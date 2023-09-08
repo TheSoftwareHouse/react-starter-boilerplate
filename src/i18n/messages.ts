b3c83c0a11bea1548cc6dee846f77be82e0fdd05
SkrzypMajster <skrzypmajster@gmail.com>
2023-09-08 07:17:10 +0200

    build: [ZN-504] Run linter

--- src/i18n/messages.ts
+++ src/i18n/messages.ts
@@ -6,10 +6,13 @@ import plMessages from './data/pl.json';
 type KeyAsValue<T> = { [P in keyof T]: P };
 
 const keysToValues = <T extends Record<string, string | unknown>>(source: T): KeyAsValue<typeof source> => {
-  return (Object.keys(source) as Array<keyof T>).reduce((accumulated, current) => {
-    accumulated[current] = current;
-    return accumulated;
-  }, {} as KeyAsValue<typeof source>);
+  return (Object.keys(source) as Array<keyof T>).reduce(
+    (accumulated, current) => {
+      accumulated[current] = current;
+      return accumulated;
+    },
+    {} as KeyAsValue<typeof source>,
+  );
 };
 
 export const AppMessages = {
