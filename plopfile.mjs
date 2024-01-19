5d06ee67b9d5b22af49201df5b020eb196b7b9a8
SkrzypMajster <skrzypmajster@gmail.com>
2024-01-19 16:08:05 +0100

    feat: separate React context generator from plopfile

--- plopfile.mjs
+++ plopfile.mjs
@@ -19,62 +19,7 @@ import {
 } from './plop/generators/apiActionsCollection.mjs';
 import { apiQueryGeneratorDescription, apiQueryGenerator } from './plop/generators/apiQuery.mjs';
 import { apiMutationGeneratorDescription, apiMutationGenerator } from './plop/generators/apiMutation.mjs';
-
-const componentTypes = {
-  API_MUTATION: '',
-  REACT_CONTEXT: 'React Context',
-};
-
-const reactContextGenerator = () => ({
-  description: componentTypes.REACT_CONTEXT,
-  prompts: [
-    {
-      type: 'input',
-      name: 'name',
-      message: 'context name',
-      validate: input => input.length > 1 || 'Context name cannot be empty!',
-    }
-  ],
-  actions: function() {
-    return [
-      {
-        type: 'add',
-        path: 'src/context/{{camelCase name}}/{{camelCase name}}Context/{{pascalCase name}}Context.ts',
-        templateFile: 'plop-templates/context/Context.hbs',
-      },
-      {
-        type: 'add',
-        path: 'src/context/{{camelCase name}}/{{camelCase name}}Context/{{pascalCase name}}Context.types.ts',
-        templateFile: 'plop-templates/context/Context.types.hbs',
-      },
-      {
-        type: 'add',
-        path: 'src/context/{{camelCase name}}/{{camelCase name}}Context/{{pascalCase name}}Context.test.tsx',
-        templateFile: 'plop-templates/context/Context.test.hbs',
-      },
-      {
-        type: 'add',
-        path: 'src/context/{{camelCase name}}/{{camelCase name}}ContextController/{{pascalCase name}}ContextController.tsx',
-        templateFile: 'plop-templates/context/ContextController.hbs',
-      },
-      {
-        type: 'add',
-        path: 'src/context/{{camelCase name}}/{{camelCase name}}ContextController/{{pascalCase name}}ContextController.types.ts',
-        templateFile: 'plop-templates/context/ContextController.types.hbs',
-      },
-      {
-        type: 'add',
-        path: 'src/hooks/use{{pascalCase name}}/use{{pascalCase name}}.ts',
-        templateFile: 'plop-templates/context/useContext.hbs',
-      },
-      {
-        type: 'add',
-        path: 'src/hooks/use{{pascalCase name}}/use{{pascalCase name}}.test.tsx',
-        templateFile: 'plop-templates/context/useContext.test.hbs',
-      },
-    ];
-  }
-});
+import { reactContextGeneratorDescription, reactContextGenerator } from './plop/generators/reactContext.mjs';
 
 export default function(plop) {
   const toKebabCase = plop.getHelper('kebabCase');
@@ -87,5 +32,5 @@ export default function(plop) {
   plop.setGenerator(apiActionsCollectionGeneratorDescription, apiActionsCollectionGenerator);
   plop.setGenerator(apiQueryGeneratorDescription, apiQueryGenerator(toKebabCase));
   plop.setGenerator(apiMutationGeneratorDescription, apiMutationGenerator(toKebabCase));
-  plop.setGenerator(componentTypes.REACT_CONTEXT, reactContextGenerator());
+  plop.setGenerator(reactContextGeneratorDescription, reactContextGenerator);
 };
