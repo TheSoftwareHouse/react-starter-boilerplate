bac05e66498521bc5724d03ba3dfe8481fcb2b15
MieszkoTSH <165779447+MieszkoTSH@users.noreply.github.com>
2024-04-11 12:12:13 +0200

    feat: add interface for logger (#150)

---
+++ src/integrations/logger.ts
@@ -0,0 +1,33 @@
+import { init, captureException, captureMessage, browserTracingIntegration } from '@sentry/browser';
+import { httpClientIntegration, captureConsoleIntegration } from '@sentry/integrations';
+
+type LogLevel = 'error' | 'info' | 'warning';
+type Logger = Record<LogLevel, (message: string | Error) => void> & Record<string, unknown>;
+
+const initLogger = () =>
+  init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    integrations: [
+      httpClientIntegration({
+        failedRequestStatusCodes: [[400, 599]],
+        failedRequestTargets: [/.*/],
+      }),
+      captureConsoleIntegration(),
+      browserTracingIntegration(),
+    ],
+    tracesSampleRate: 1.0,
+  });
+
+const sendLog = (level: LogLevel, message: string | Error) => {
+  if (typeof message === 'string') {
+    captureMessage(message, { level });
+  }
+  captureException(message, { level });
+};
+
+export const logger = {
+  init: initLogger,
+  error: (message: string | Error) => sendLog('error', message),
+  warning: (message: string | Error) => sendLog('warning', message),
+  info: (message: string | Error) => sendLog('info', message),
+} satisfies Logger;
