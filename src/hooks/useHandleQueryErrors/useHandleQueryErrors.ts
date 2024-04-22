a23554ed89a173cadda81414874471f42b3a3ed0
Mieszko Kowalik <mieszko.kowalik@tsh.io>
2024-04-22 14:23:45 +0200

    feat: add error logging to useHandleQueryErrors

--- src/hooks/useHandleQueryErrors/useHandleQueryErrors.ts
+++ src/hooks/useHandleQueryErrors/useHandleQueryErrors.ts
@@ -1,17 +1,18 @@
 import { isClientError, isServerError } from 'utils/apiErrorStatuses';
 import { ApiError } from 'context/apiClient/apiClientContextController/apiError/apiError.types';
 import { ExtendedQueryMeta } from 'api/types/types';
+import { logger } from 'integrations/logger';
 
 export const useHandleQueryErrors = () => {
   const handleErrors = (error: ApiError) => {
     if (isServerError(error.statusCode)) {
       // show translated error message in toast/snackbar
-      // handle logger
+      logger.error(error.originalError.message);
     }
 
     if (isClientError(error.statusCode)) {
       // show translated error message in toast/snackbar
-      // handle logger
+      logger.error(error.originalError.message);
     }
   };
 
