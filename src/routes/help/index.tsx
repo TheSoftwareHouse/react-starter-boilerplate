b0f4f8e614248d54c0fa2f160ad3a75d8927c071
tokerson <kuba.tokarzewski@gmail.com>
2024-05-06 18:22:46 +0200

    chore: remove old index.ts file for the help page

--- src/routes/help/index.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-
-import { Help } from 'routes/help/-components/Help';
-
-export const Route = createFileRoute('/help/')({
-  component: () => <Help />,
-});
